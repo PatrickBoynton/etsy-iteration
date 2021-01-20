6b741093ee6ccc8608a1c7729ff4da06e91f3b9b
PatrickBoynton <jpboynton3@gmail.com>
2021-01-20 16:35:55 -0500

    Commented out code.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Question 1.
-// const prices = [];
+const prices = [];
 
 // items.forEach((item) => {
 //   prices.push(item.price);
@@ -13,12 +13,13 @@
 
 // Question 2.
 // const usd = items.filter(x => x.currency_code === "USD" && x.price > 14.00 && x.price < 18.00);
-//
+// console.log("tems that cost between $14.00 USD and $18.00 USD: \n");
 // usd.forEach((item) => {
-//   prices.push(item.title)
+//   prices.push(item.title);
 // });
-//
-// console.log(prices);
+// prices.forEach((item) => {
+//   console.log(item);
+// });
 
 // Quesetion 3.
 // const gbp = items.filter(x => x.currency_code === "GBP");
@@ -32,10 +33,15 @@
 //     console.log(`${item.title} is made of wood.`);
 // });
 
-//  Quesetion.
-const greaterThanEight = items.filter(x => x.materials.length > 7);
+//  Quesetion 5.
+// const greaterThanEight = items.filter(x => x.materials.length > 7);
+//
+// greaterThanEight.forEach((item) => {
+//     console.log(`${item.title}\n\n`);
+//     console.log(`${item.materials}` + "\n");
+// });
 
-greaterThanEight.forEach((item) => {
-    console.log(`${item.title}\n\n`);
-    console.log(`${item.materials}` + "\n");
-});
+// Question 6.
+// const whoMade = items.filter(x => x.who_made === "i_did");
+//
+// console.log(`${whoMade.length} were made by their sellers.`);
