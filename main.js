be9ed65c5b749ca8170bf30ffdde7de7e1c1f8da
PatrickBoynton <jpboynton3@gmail.com>
2021-01-20 14:47:47 -0500

    Solved question 2.

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 // Question 1.
 const prices = [];
 
-items.forEach((item) => {
-  prices.push(item.price);
+// items.forEach((item) => {
+//   prices.push(item.price);
+// });
+
+//
+// const averagePrice = prices.reduce((a, b) => a + b) / items.length;
+//
+// console.log(`The average price is: $${averagePrice.toFixed(2)}`);
+
+
+// Question 2.
+const usd = items.filter(x => x.currency_code === "USD" && x.price > 14.00 && x.price < 18.00);
+
+usd.forEach((item) => {
+  prices.push(item.title)
 });
 
-
-const averagePrice = prices.reduce((a, b) => a + b) / items.length;
-
-console.log(`The average price is: $${averagePrice.toFixed(2)}`);
+console.log(prices);
